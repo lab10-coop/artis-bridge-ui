c20a8bbd890bcd349af42a3a6a3f736c396e5ce2
Thomas Haller <Thomas.Haller@lab10.coop>
2019-02-23 16:18:27 +0100

    ARTIS theme, further improvements on the mainpage

--- src/components/BridgeAddress.js
+++ src/components/BridgeAddress.js
@@ -10,9 +10,7 @@ export const BridgeAddress = ({ isHome, reverse, labelName}) => {
     (<div className="bridge-home">
       <div className="bridge-home-container">
         <div className="home-logo-container">
-          <div className={reverse ? 'foreign-logo' : 'home-logo'}
-            style={{color: 'white', textAlign: 'center', fontWeight: 'bold'}}
-          >
+          <div className={reverse ? 'foreign-logo' : 'home-logo'}>
             {labelName}
           </div>
         </div>
@@ -24,9 +22,7 @@ export const BridgeAddress = ({ isHome, reverse, labelName}) => {
       {getAddress()}
       <div className="bridge-foreign-container">
         <div className="foreign-logo-container">
-          <div className={reverse ? 'foreign-logo' : 'home-logo'}
-               style={{color: 'white', textAlign: 'center', fontWeight: 'bold'}}
-          >
+          <div className={reverse ? 'foreign-logo' : 'home-logo'}>
             {labelName}
           </div>
         </div>
