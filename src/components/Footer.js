804bd2254b2224ad1bf569e5c90e7cf38cc54c1a
Thomas Haller <Thomas.Haller@lab10.coop>
2019-02-27 21:38:18 +0100

    ARTIS social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,14 @@ import React from 'react';
 export const Footer = () => (
   <footer className="footer">
     <div className="container">
-      <p className="footer-rights">2018 POA. All rights reserved.</p>
+      <p className="footer-rights">2018 POA &amp; lab10. All rights reserved.</p>
       <a href="/"><i className="footer-logo" /></a>
       <div className="socials">
+        <a href="https://t.me/joinchat/JEYz2EsBddOZr1pXVkpK8g" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_telegram" /></a>
+        <a href="https://medium.com/lab10-collective" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_medium" /></a>
+        <a href="https://github.com/lab10-coop/" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_github" /></a>
+        <a href="https://www.linkedin.com/company/lab10-coop/" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_linkedin" /></a>
         <a href="https://twitter.com/poanetwork" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_twitter" /></a>
-        <a href="https://t.me/poa_network" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_telegram" /></a>
-        <a href="https://github.com/poanetwork/token-bridge" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_github" /></a>
       </div>
     </div>
   </footer>
