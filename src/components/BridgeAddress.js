4419d946809161a5b4b8d595706e7076a3636f0b
Thomas Haller <Thomas.Haller@lab10.coop>
2019-03-01 17:54:42 +0100

    using coin svg icons on bridge element instead of text

--- src/components/BridgeAddress.js
+++ src/components/BridgeAddress.js
@@ -1,17 +1,23 @@
 import React from 'react'
+import coin_ats from '../assets/images/logos/coin_ats.svg';
+import coin_ats20 from '../assets/images/logos/coin_ats20.svg';
 
 export const BridgeAddress = ({ isHome, reverse, labelName}) => {
   const getAddress = () => isHome ?
     (<div className="home-address-container" />)
     :
     (<div className="foreign-address-container" />)
+  
+  var coin = labelName
+  if (labelName == 'ATS') coin = <img src={coin_ats} />
+  if (labelName == 'ATS20') coin = <img src={coin_ats20} />
 
   return isHome ?
     (<div className="bridge-home">
       <div className="bridge-home-container">
         <div className="home-logo-container">
           <div className={reverse ? 'foreign-logo' : 'home-logo'}>
-            {labelName}
+           {coin}
           </div>
         </div>
       </div>
@@ -23,7 +29,7 @@ export const BridgeAddress = ({ isHome, reverse, labelName}) => {
       <div className="bridge-foreign-container">
         <div className="foreign-logo-container">
           <div className={reverse ? 'foreign-logo' : 'home-logo'}>
-            {labelName}
+            {coin}
           </div>
         </div>
       </div>
