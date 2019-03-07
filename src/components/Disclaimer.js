1fdeb115ce9874f1199a2cebf6a9b7e16260ef7b
Thomas Haller <Thomas.Haller@lab10.coop>
2019-03-07 12:12:01 +0100

    integration of legal disclaimer

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -7,25 +7,24 @@ export const Disclaimer = ({ onConfirmation }) => (
       <img className="disclaimer-icon" src={disclaimerIcon} alt="disclaimer icon"/>
     </div>
     <div className="alert-container">
-      <span className="disclaimer-title">Welcome to <br/> POA Bridge UI App Beta+</span>
+      <span className="disclaimer-title">Welcome to  <br/>  ARTIS Bridge DApp (Beta)</span>
       <p className="disclaimer-description">
-        We’re launching our POA Bridge and our UI App on a beta-testing basis. While we’ve
-        worked long and hard to develop the core features of the software, we expect that our
-        users may detect bugs and other issues. Help us improve by submitting tickets to our
-        <a href="https://poanet.zendesk.com/hc/en-us/categories/360000349273-POA-Bridge" target="_blank"
-           rel="noopener noreferrer"> support page</a>.
-        <br/>
-        <br/>
-        Use of this app and the POA Bridge is at your own risk. Users may experience
-        unexpected delays, unexpected visual artifacts, unexpected loss of tokens or funds from
-        improper app configuration, or other negative outcomes.
-        <br/>
-        <br/>
-        By hitting the “continue” button, you are representing that you’ve read our <a
-        href="https://poanet.zendesk.com/hc/en-us/articles/360004054794-Terms-and-Conditions-of-The-POA-Bridge"
-        target="_blank" rel="noopener noreferrer">Terms of
-        Service</a> in full, and that you agree to be legally bound by them.
+      The ARTIS Bridge DApp is built to allow a convenient and fast way to transfer assets between blockchains.
+      The development effort is led by POA Network and is currently in beta-testing status and you can find
+      further information about this implementation on the&nbsp;
+      <a href="https://github.com/poanetwork/token-bridge" target="_blank" rel="noopener noreferrer">POA Github</a>
+      . ARTIS Bridge specific changes can be found on the&nbsp; 
+      <a  href="https://github.com/lab10-coop/" target="_blank" rel="noopener noreferrer">lab10 collective Github</a>
+      .<br /><br />
+      Use of this app and the ARTIS Bridge is at your own risk. 
+      Users may experience unexpected delays, unexpected visual artifacts, unexpected loss of tokens or funds
+      from improper app configuration, or other negative outcomes.
+      <br/><br />
+      By hitting the “continue” button, 
+      you are representing that you’ve read our 
+      <a href="https://wiki.lab10.io/display/artisP/ARTIS+Bridge+Legal+Disclaimer" target="_blank" rel="noopener noreferrer"> Terms of Service</a> in full, and that you agree to be legally bound by them.
       </p>
+
       <div className="disclaimer-buttons">
         <button
           className="disclaimer-confirm"
