89661b853b3b747bb1f42d8bd4c78611d309c62f
Thomas Haller <Thomas.Haller@lab10.coop>
2019-03-08 12:17:48 +0100

    fixed disclaimer size problem

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -15,11 +15,11 @@ export const Disclaimer = ({ onConfirmation }) => (
       <a href="https://github.com/poanetwork/token-bridge" target="_blank" rel="noopener noreferrer">POA Github</a>
       . ARTIS Bridge specific changes can be found on the&nbsp; 
       <a  href="https://github.com/lab10-coop/" target="_blank" rel="noopener noreferrer">lab10 collective Github</a>
-      .<br /><br />
+      .<br />
       Use of this app and the ARTIS Bridge is at your own risk. 
       Users may experience unexpected delays, unexpected visual artifacts, unexpected loss of tokens or funds
       from improper app configuration, or other negative outcomes.
-      <br/><br />
+      <br/>
       By hitting the “continue” button, 
       you are representing that you’ve read our 
       <a href="https://wiki.lab10.io/display/artisP/ARTIS+Bridge+Legal+Disclaimer" target="_blank" rel="noopener noreferrer"> Terms of Service</a> in full, and that you agree to be legally bound by them.
